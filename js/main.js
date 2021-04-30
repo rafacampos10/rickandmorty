e6a6051090819c02c7e2ea8b8ac58b8b0c04b41f
Rafael Campos Martins <rafinhasep94@hotmail.com>
2021-04-30 11:10:47 -0300

    upgrade

--- js/main.js
+++ js/main.js
@@ -2,6 +2,64 @@ document.getElementById('formulario').addEventListener('submit', pesquisarPerson
 
 function pesquisarPersonagem (e){
 	
-	console.log('Hello World');
+	var personagemPesquisar = document.getElementById('pesquisar').value;
+    buscarpersonagem(personagemPesquisar);
 	e.preventDefault();
+}
+
+function buscarpersonagem(personagemPesquisar){
+
+// Procura por nome
+axios.get('https://rickandmortyapi.com/api/character/?name=' + personagemPesquisar)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    var personagem = response.data.results;
+     var mostrarPersonagem = '';
+    
+    for(var i =0;i<personagem.length; i++){
+        mostrarPersonagem += `<div class ="col-sm-6 col-md-4">
+<div class="thumbnail">
+<img src="${personagem[i].image}" class="img-thumbnail">
+</div>
+</div>`
+    }
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+// Procura por status
+ axios.get('https://rickandmortyapi.com/api/character/?status=' + personagemPesquisar)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+     var personagem = response.data.results;
+     var mostrarPersonagem = '';
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+    
+    //Procura por gênero
+    axios.get('https://rickandmortyapi.com/api/character/?gender=' + personagemPesquisar)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 }
